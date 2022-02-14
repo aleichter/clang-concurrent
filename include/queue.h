c5a41edce123c64fa1e52dc6c92619004f60402e
Andrew Leichter <andrew.leichter@epsoftinc.com>
2022-02-14 17:51:31 -0500

    queue logic is created and test cases are implemented

--- include/queue.h
+++ include/queue.h
@@ -1,6 +1,6 @@
 struct node {
     int value;
-    node* next;
+    node *next;
 };
 
 struct queue {
@@ -11,4 +11,36 @@ struct queue* createQueue() {
     struct queue* q = (struct queue*)malloc(sizeof(struct queue));
     q->head = q->tail = NULL;
     return q;
+};
+
+void enqueue(struct queue *q, int v) {
+    struct node *tempNode = (struct node*)malloc(sizeof(struct node));
+    tempNode->value = v;
+    tempNode->next = NULL;
+
+    if(q->tail == NULL) {
+        q->head = q->tail = tempNode;
+        return;
+    }
+
+    q->tail->next = tempNode;
+    q->tail = tempNode;
+};
+
+void dequeue(struct queue *q) {
+    if(q->head == NULL)
+        return;
+    
+    struct node *tempNode = q->head;
+    q->head = q->head->next;
+    if(q->head == NULL)
+        q->tail = NULL;
+
+    free(tempNode);
+};
+
+void emptyQueue(struct queue *q) {
+    while(q->head != NULL) {
+        dequeue(q);
+    }
 };
--- tests/test_queue.cpp
+++ tests/test_queue.cpp
@@ -1,17 +1,81 @@
 #include <gtest/gtest.h>
 
 extern "C" {
-#include "../include/queue.h"
+    #include "../include/queue.h"
 }
 
-TEST(queueTests, queueAllocation)
+//Test for createQueue
+TEST(queueTests, createQueue)
 {
-    queue *q;
+    struct queue *q;
     q = createQueue();
     EXPECT_EQ(q->head, nullptr);
     EXPECT_EQ(q->tail, nullptr);
-    //TODO:  Need a freeQueue function as before we free the q pointer
-    // we need to free all the pointers in the linked list to nodes.
-    // other wise we'll end up with memory leaks
+    free(q);
+}
+
+//Test for enqueue
+TEST(queueTests, enqueue) {
+    struct queue *q;
+    q = createQueue();
+    enqueue(q, 1);
+    struct node *node1 = q->head;
+    EXPECT_NE(node1, nullptr);
+    EXPECT_EQ(node1->value, 1);
+    EXPECT_EQ(q->head, node1);
+    EXPECT_EQ(q->tail, node1);
+
+    enqueue(q, 2);
+    struct node *node2 = q->tail;
+    EXPECT_NE(node2, nullptr);
+    EXPECT_EQ(node2->value, 2);
+    EXPECT_EQ(q->head, node1);
+    EXPECT_EQ(q->tail, node2);
+
+    free(q);
+}
+
+//Test for dequeue
+TEST(queueTests, dequeue) {
+    struct queue *q;
+    q = createQueue();
+    enqueue(q, 1);
+    struct node *node1 = q->tail;
+    enqueue(q, 2);
+    struct node *node2 = q->tail;
+    enqueue(q, 3);
+    struct node *node3 = q->tail;
+
+    EXPECT_NE(node1, nullptr);
+    EXPECT_NE(node2, nullptr);
+    EXPECT_NE(node3, nullptr);
+    EXPECT_EQ(q->head, node1);
+    EXPECT_EQ(q->tail, node3);
+
+    dequeue(q);
+
+    EXPECT_EQ(q->head, node2);
+    EXPECT_EQ(q->tail, node3);
+
+    free(q);
+}
+
+//Test for destroyQueue
+TEST(queueTests, emptyQueue) {
+    struct queue *q;
+    q = createQueue();
+    enqueue(q, 1);
+    struct node *node1 = q->tail;
+    enqueue(q, 2);
+    struct node *node2 = q->tail;
+    enqueue(q, 3);
+    struct node *node3 = q->tail;
+
+    EXPECT_NE(q->head, nullptr);
+
+    emptyQueue(q);
+
+    EXPECT_EQ(q->head, nullptr);
+
     free(q);
 }
